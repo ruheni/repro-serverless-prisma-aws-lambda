0b920e64f2ea34710556310eaec942ff678a83f2
ruheni <aleruheni@outlook.com>
2022-01-17 10:03:10 +0100

    project init

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path')
+const nodeExternals = require('webpack-node-externals')
+const slsw = require('serverless-webpack')
+const { isLocal } = slsw.lib.webpack
+
+module.exports = {
+  target: 'node',
+  stats: 'normal',
+  entry: slsw.lib.entries,
+  externals: [nodeExternals()],
+  mode: isLocal ? 'development' : 'production',
+  resolve: { extensions: ['.js'] },
+  output: {
+    libraryTarget: 'commonjs',
+    filename: '[name].js',
+    path: path.resolve(__dirname, '.webpack'),
+  },
+}
